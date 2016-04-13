0d06494d8b0cc101b297c3a361ee7c64a688e614
jaybau <jbauson@gmail.com>
2016-04-14 04:39:56 +0800

    Added update

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -2,15 +2,40 @@ var app = angular.module('myApp', []);
 
 app.controller('AppCtrl', ['$scope','$http', function($scope,$http){
     // Call API Server
-    $http.get('/contactlist').success(function(response){
-        console.log(response);
-        $scope.contactlist = response;
-    });
+    function refresh(){
+        $http.get('/contactlist').success(function(response){
+            $scope.contactlist = response;
+        });
+    }
+    refresh();
 
     $scope.addContact = function(){
         console.log($scope.contact);
-        $http.post('/contactlist',$scope.contact);
-    }
+        $http.post('/contactlist',$scope.contact).success(function(response){
+            $scope.contactlist.push(response);
+            $scope.contact = "";
+        })
+    };
+
+    $scope.removeContact = function(id){
+        $http.delete('/contactlist/'+id).success(function(response){
+            refresh();
+        })
+    };
+
+    $scope.editContact = function(id){
+        $http.get('/contactlist/'+id).success(function(response){
+            $scope.contact = response;
+        })
+    };
+
+    $scope.updateContact = function(id){
+        $http.put('/contactlist/'+$scope.contact._id, $scope.contact).success(function(response){
+            refresh();
+            $scope.contact = "";
+        });
+    };
+
 
 }]);
 
